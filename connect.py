9aec395e467a8dd861da0126ef0306d649ed6b42
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-02-24 00:07:28 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,2 +1,9 @@
+#connect python with webbrowser chrome
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import pyautogui as pag
+
+def login():
+
+    #getting the login element
+    
