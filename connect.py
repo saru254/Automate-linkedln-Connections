539dd4b3414de1f03597a7fbe10a0b8237f7d029
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-02-24 00:12:10 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -30,3 +30,20 @@ def send_requests():
         # position(in px) of connection button
         pag.click(880, 770)  
     print("Done !")
+
+
+def main():
+ 
+    # url of LinkedIn
+    url = "http://linkedin.com/" 
+ 
+    # url of LinkedIn network page
+    network_url = "http://linkedin.com / mynetwork/"  
+ 
+    # path to browser web driver
+    driver = webdriver.Chrome('C:\\Program Files\\Web Driver\\chromedriver.exe')     
+    driver.get(url)
+ 
+# Driver's code 
+if __name__ == __main__:
+    main()
