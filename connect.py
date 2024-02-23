07b9ebc0a8d44dde7a8d9900de9aa4a375280a40
Karen Meni <107751782+saru254@users.noreply.github.com>
2024-02-24 00:10:12 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -6,4 +6,15 @@ import pyautogui as pag
 def login():
 
     #getting the login element
+    username = driver.find_element_by_id("login-email")
+
+    #sending the keys for username
+    username.send_keys("username")
+
+    #sending the keys for password
+    password.send_keys("password")
+
+    #getting the tag for submit button
+    driver.find_element_by_id("login-submit").click()
+
     
